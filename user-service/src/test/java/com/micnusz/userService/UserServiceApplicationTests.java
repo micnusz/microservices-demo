86906b9eca882fbaa1508a3b4f1db9311998099e
Michal <michalnuszkiewiczbusiness@gmail.com>
2026-04-01 15:04:35 +0200

    Added api-gateway, user-service

---
+++ user-service/src/test/java/com/micnusz/userService/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micnusz.userService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
